9a2e8cdc78d42dfa3eba2a288e3adcf4cf366470
李钦 <sylvia.lannister@outlook.com>
2020-10-30 09:55:08 +0800

    get

--- main/java/com/example/easyreader/MainActivity.java
+++ main/java/com/example/easyreader/MainActivity.java
@@ -4,11 +4,78 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+import static java.net.Proxy.Type.HTTP;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String msg = "";
+        String urlStr = "http://123.56.151.219/tree/tree";
+        try {
+            URL url = new URL(urlStr);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setConnectTimeout(6000);
+            InputStream in = conn.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                if (msg == null) {
+                    msg = line;
+                } else {
+                    msg += line;
+                }
+            }
+            reader.close();
+            in.close();//关闭数据流
+            conn.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("123----------------------------------------------------------------------");
+        System.out.println(msg);
+
+
+        /*
+        String strResult = "";
+        String strUrlPath = "http://123.56.151.219/tree/tree";
+        try {
+            URL url = new URL(strUrlPath);
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setUseCaches(false);
+            conn.setConnectTimeout(10000);
+            conn.setReadTimeout(10000);
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"));
+            StringBuffer buffer = new StringBuffer();
+            String line = "";
+            while ((line = in.readLine()) != null){
+                buffer.append(line);
+            }
+            strResult = buffer.toString();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(strResult);
+        */
     }
 }
